248339f427b3c607bd8fa441bc791f015645ef13
tomitobio <tomasetobio@gmail.com>
2023-10-10 23:31:57 -0300

    notificable

---
+++ Java/Interface/SistemaDeNotificacion/SistemaNotificacion.java
@@ -0,0 +1,51 @@
+package SistemaDeNotificacion;
+
+interface Notificable {
+    void enviarNotificacion(int tipo);
+}
+
+class CorreoElectronico implements Notificable {
+    // Atributos
+    private String casilla;
+    private String contra;
+
+    // Constructor
+    public CorreoElectronico(String casilla, String contra) {
+        this.casilla = casilla;
+        this.contra = contra;
+    }
+
+    // Notificable
+    @Override
+    public void enviarNotificacion(int tipo) {
+        if ((tipo > 0) && (tipo <= 4)) {
+            switch (tipo) {
+                case 1:
+                    System.out.println("Mail o contraseña erróneos, vuelva a intentar.");
+                    break;
+                case 2:
+                    System.out.println("Bienvenido al sistema.");
+                    break;
+                case 3:
+                    System.out.println("Mail enviado.");
+                    break;
+                case 4:
+                    System.out.println("Mail recibido.");
+                    break;
+            }
+        } else {
+            System.out.println("Este numero no es un tipo de notificacion");
+        }
+
+    }
+}
+
+public class SistemaNotificacion {
+    public static void main(String[] args) {
+        CorreoElectronico correo1 = new CorreoElectronico("tomastobio", "44851196");
+        boolean recibioCorreo = true;
+        if (recibioCorreo) {
+            correo1.enviarNotificacion(4);
+        }
+    }
+}
