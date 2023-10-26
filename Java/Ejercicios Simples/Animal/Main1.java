92c5ce533e8985bb9d03a5a1975db3442857ee52
tomitobio <tomasetobio@gmail.com>
2023-10-26 20:37:46 -0300

    tt

--- Java/Ejercicios Simples/Animal/Main1.java
+++ Java/Ejercicios Simples/Animal/Main1.java
@@ -1,19 +1,109 @@
-// Definir la clase abstracta Animal
-abstract class Animal {
+// // Definir la clase abstracta Animal
+// abstract class Animal {
 
+// private String nombre;
+// private int edad;
+
+// // Constructor
+// public Animal(String nombre, int edad) {
+// this.nombre = nombre;
+// this.edad = edad;
+// }
+
+// // Metodo clase abstracta
+// public abstract void hacerSonido();
+
+// // Get y set
+// public String getNombre() {
+// return nombre;
+// }
+
+// public int getEdad() {
+// return edad;
+// }
+// }
+
+// // Definir la interfaz Mascota
+// interface Mascota {
+// void jugar();
+
+// void comer();
+// }
+
+// // Clase concreta Perro que extiende Animal e implementa Mascota
+// class Perro extends Animal implements Mascota {
+// // Constructor con clase abstracta
+// public Perro(String nombre, int edad) {
+// super(nombre, edad);
+// }
+
+// // Metodo clase abs
+// @Override
+// public void hacerSonido() {
+// System.out.println("¡Guau!");
+// }
+
+// // Metodos interface
+// @Override
+// public void jugar() {
+// System.out.println(getNombre() + " está jugando.");
+// }
+
+// @Override
+// public void comer() {
+// System.out.println(getNombre() + " está comiendo.");
+// }
+// }
+
+// // Clase concreta Gato que extiende Animal e implementa Mascota
+// class Gato extends Animal implements Mascota {
+// // Constructor con clase abstracta
+// public Gato(String nombre, int edad) {
+// super(nombre, edad);
+// }
+
+// // Metodo clase abs
+// @Override
+// public void hacerSonido() {
+// System.out.println("¡Miau!");
+// }
+
+// // Metodos interface
+// @Override
+// public void jugar() {
+// System.out.println(getNombre() + " está jugando.");
+// }
+
+// @Override
+// public void comer() {
+// System.out.println(getNombre() + " está comiendo.");
+// }
+// }
+
+// public class Main1 {
+// public static void main(String[] args) {
+// Perro miPerro = new Perro("Rex", 3);
+// Gato miGato = new Gato("Whiskers", 2);
+
+// miPerro.hacerSonido(); // Salida: ¡Guau!
+// miPerro.jugar(); // Salida: Rex está jugando.
+// miPerro.comer(); // Salida: Rex está comiendo.
+
+// miGato.hacerSonido(); // Salida: ¡Miau!
+// miGato.jugar(); // Salida: Whiskers está jugando.
+// miGato.comer(); // Salida: Whiskers está comiendo.
+// }
+// }
+
+abstract class Animal {
     private String nombre;
     private int edad;
 
-    // Constructor
     public Animal(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
     }
 
-    // Metodo clase abstracta
-    public abstract void hacerSonido();
-
-    // Get y set
     public String getNombre() {
         return nombre;
     }
@@ -21,76 +111,74 @@ abstract class Animal {
     public int getEdad() {
         return edad;
     }
+
+    public abstract void hacerSonido();
 }
 
-// Definir la interfaz Mascota
 interface Mascota {
     void jugar();
 
     void comer();
 }
 
-// Clase concreta Perro que extiende Animal e implementa Mascota
-class Perro extends Animal implements Mascota {
-    // Constructor con clase abstracta
-    public Perro(String nombre, int edad) {
-        super(nombre, edad);
-    }
-
-    // Metodo clase abs
-    @Override
-    public void hacerSonido() {
-        System.out.println("¡Guau!");
-    }
-
-    // Metodos interface
-    @Override
-    public void jugar() {
-        System.out.println(getNombre() + " está jugando.");
-    }
-
-    @Override
-    public void comer() {
-        System.out.println(getNombre() + " está comiendo.");
-    }
-}
-
-// Clase concreta Gato que extiende Animal e implementa Mascota
 class Gato extends Animal implements Mascota {
-    // Constructor con clase abstracta
     public Gato(String nombre, int edad) {
         super(nombre, edad);
     }
 
-    // Metodo clase abs
     @Override
     public void hacerSonido() {
         System.out.println("¡Miau!");
     }
 
-    // Metodos interface
     @Override
     public void jugar() {
-        System.out.println(getNombre() + " está jugando.");
+        System.out.println(getNombre() + "esta jugando!");
     }
 
     @Override
     public void comer() {
-        System.out.println(getNombre() + " está comiendo.");
+        System.out.println(getNombre() + "esta comiendo!");
+    }
+}
+
+class Perro extends Animal implements Mascota {
+    public Perro(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println("Guau!");
+    }
+
+    @Override
+    public void jugar() {
+        System.out.println(getNombre() + "esta jugando!");
+    }
+
+    @Override
+    public void comer() {
+        System.out.println(getNombre() + "esta comiendo!");
     }
 }
 
 public class Main1 {
     public static void main(String[] args) {
-        Perro miPerro = new Perro("Rex", 3);
-        Gato miGato = new Gato("Whiskers", 2);
+        Gato cat = new Gato("Obi", 2);
+        System.out.println(cat.getNombre());
+        System.out.println(cat.getEdad());
 
-        miPerro.hacerSonido(); // Salida: ¡Guau!
-        miPerro.jugar(); // Salida: Rex está jugando.
-        miPerro.comer(); // Salida: Rex está comiendo.
+        Perro dog = new Perro("Sony", 5);
+        System.out.println(dog.getNombre());
+        System.out.println(dog.getEdad());
 
-        miGato.hacerSonido(); // Salida: ¡Miau!
-        miGato.jugar(); // Salida: Whiskers está jugando.
-        miGato.comer(); // Salida: Whiskers está comiendo.
+        cat.hacerSonido();
+        cat.jugar();
+        cat.comer();
+
+        dog.hacerSonido();
+        dog.jugar();
+        dog.comer();
     }
 }
--- Java/Ejercicios Simples/CuentaBanca/Main2.java
+++ Java/Ejercicios Simples/CuentaBanca/Main2.java
@@ -1,72 +1,122 @@
-// Clase base CuentaBancaria
-class CuentaBancaria {
-    private String numeroCuenta;
-    protected double saldo;
+// // Clase base CuentaBancaria
+// class CuentaBancaria {
+//     private String numeroCuenta;
+//     protected double saldo;
 
-    public CuentaBancaria(String numeroCuenta, double saldoInicial) {
-        this.numeroCuenta = numeroCuenta;
-        this.saldo = saldoInicial;
-    }
+//     public CuentaBancaria(String numeroCuenta, double saldoInicial) {
+//         this.numeroCuenta = numeroCuenta;
+//         this.saldo = saldoInicial;
+//     }
 
-    public void depositar(double cantidad) {
-        saldo += cantidad;
-        System.out.println("Depósito de $" + cantidad + " realizado. Saldo actual: $" + saldo);
-    }
+//     public void depositar(double cantidad) {
+//         saldo += cantidad;
+//         System.out.println("Depósito de $" + cantidad + " realizado. Saldo actual: $" + saldo);
+//     }
 
-    public void retirar(double cantidad) {
-        if (cantidad <= saldo) {
-            saldo -= cantidad;
-            System.out.println("Retiro de $" + cantidad + " realizado. Saldo actual: $" + saldo);
-        } else {
-            System.out.println("Saldo insuficiente para retirar $" + cantidad);
-        }
+//     public void retirar(double cantidad) {
+//         if (cantidad <= saldo) {
+//             saldo -= cantidad;
+//             System.out.println("Retiro de $" + cantidad + " realizado. Saldo actual: $" + saldo);
+//         } else {
+//             System.out.println("Saldo insuficiente para retirar $" + cantidad);
+//         }
+//     }
+
+//     public void mostrarSaldo() {
+//         System.out.println("Saldo actual en la cuenta " + numeroCuenta + ": $" + saldo);
+//     }
+// }
+
+// // Subclase CuentaCorriente
+// class CuentaCorriente extends CuentaBancaria {
+//     public CuentaCorriente(String numeroCuenta, double saldoInicial) {
+//         super(numeroCuenta, saldoInicial);
+//     }
+// }
+
+// // Subclase CuentaAhorro
+// class CuentaAhorro extends CuentaBancaria {
+//     public CuentaAhorro(String numeroCuenta, double saldoInicial) {
+//         super(numeroCuenta, saldoInicial);
+//     }
+
+//     @Override
+//     public void retirar(double cantidad) {
+//         if (cantidad <= saldo) {
+//             saldo -= cantidad;
+//             System.out.println(
+//                     "Retiro de $" + cantidad + " desde la cuenta de ahorro realizado. Saldo actual: $" + saldo);
+//         } else {
+//             System.out.println("Saldo insuficiente para retirar $" + cantidad + " desde la cuenta de ahorro.");
+//         }
+//     }
+// }
+
+// public class Main2 {
+//     public static void main(String[] args) {
+//         CuentaCorriente cuentaCorriente = new CuentaCorriente("123456", 1000);
+//         CuentaAhorro cuentaAhorro = new CuentaAhorro("789012", 2000);
+
+//         cuentaCorriente.mostrarSaldo(); // Saldo actual en la cuenta 123456: $1000.0
+//         cuentaAhorro.mostrarSaldo(); // Saldo actual en la cuenta 789012: $2000.0
+
+//         cuentaCorriente.depositar(500); // Depósito de $500.0 realizado. Saldo actual: $1500.0
+//         cuentaCorriente.retirar(200); // Retiro de $200.0 realizado. Saldo actual: $1300.0
+
+//         cuentaAhorro.depositar(300); // Depósito de $300.0 realizado. Saldo actual: $2300.0
+//         cuentaAhorro.retirar(2500); // Saldo insuficiente para retirar $2500.0 desde la cuenta de ahorro.
+
+//         cuentaCorriente.mostrarSaldo(); // Saldo actual en la cuenta 123456: $1300.0
+//         cuentaAhorro.mostrarSaldo(); // Saldo actual en la cuenta 789012: $2300.0
+//     }
+// }
+
+class cuentaBancaria {
+    public int numC;
+    public double saldo;
+
+    public cuentaBancaria(int numC, double saldo) {
+        this.numC = numC;
+        this.saldo = saldo;
     }
 
     public void mostrarSaldo() {
-        System.out.println("Saldo actual en la cuenta " + numeroCuenta + ": $" + saldo);
-    }
-}
-
-// Subclase CuentaCorriente
-class CuentaCorriente extends CuentaBancaria {
-    public CuentaCorriente(String numeroCuenta, double saldoInicial) {
-        super(numeroCuenta, saldoInicial);
-    }
-}
-
-// Subclase CuentaAhorro
-class CuentaAhorro extends CuentaBancaria {
-    public CuentaAhorro(String numeroCuenta, double saldoInicial) {
-        super(numeroCuenta, saldoInicial);
+        System.out.println("El saldo de la cuenta es: " + this.saldo);
     }
 
-    @Override
-    public void retirar(double cantidad) {
-        if (cantidad <= saldo) {
-            saldo -= cantidad;
-            System.out.println(
-                    "Retiro de $" + cantidad + " desde la cuenta de ahorro realizado. Saldo actual: $" + saldo);
-        } else {
-            System.out.println("Saldo insuficiente para retirar $" + cantidad + " desde la cuenta de ahorro.");
+    public void retirar(double retiro){
+        try {
+            if (retiro > saldo){
+                System.out.println("El monto ingresado es mayor al dinero disponible");
+            }
+            else {
+                saldo -= retiro;
+                System.out.println("El monto ingresado ha sido retirado.");
+            }
         }
+        catch(Exception e)    }
+
+    public void depositar(double deposito) {
+
+        saldo -= retiro;
+
     }
-}
+}}
 
 public class Main2 {
     public static void main(String[] args) {
-        CuentaCorriente cuentaCorriente = new CuentaCorriente("123456", 1000);
-        CuentaAhorro cuentaAhorro = new CuentaAhorro("789012", 2000);
+        CuentaCorriente cuenta = new cuentaCorriente(352476, 5000);
+        CuentaAhorro ahorro = new cuentaAhorro(35801, 2000);
 
-        cuentaCorriente.mostrarSaldo(); // Saldo actual en la cuenta 123456: $1000.0
-        cuentaAhorro.mostrarSaldo(); // Saldo actual en la cuenta 789012: $2000.0
+        cuenta.mostrarSaldo();
+        cuenta.retirar(2600);
+        cuenta.depositar(5600);
+        cuenta.mostrarSaldo();
 
-        cuentaCorriente.depositar(500); // Depósito de $500.0 realizado. Saldo actual: $1500.0
-        cuentaCorriente.retirar(200); // Retiro de $200.0 realizado. Saldo actual: $1300.0
+        ahorro.mostrarSaldo();
+        ahorro.retirar(700);
+        ahorro.depositar(8200);
+        ahorro.mostrarSaldo();
 
-        cuentaAhorro.depositar(300); // Depósito de $300.0 realizado. Saldo actual: $2300.0
-        cuentaAhorro.retirar(2500); // Saldo insuficiente para retirar $2500.0 desde la cuenta de ahorro.
-
-        cuentaCorriente.mostrarSaldo(); // Saldo actual en la cuenta 123456: $1300.0
-        cuentaAhorro.mostrarSaldo(); // Saldo actual en la cuenta 789012: $2300.0
     }
 }
