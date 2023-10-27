9cefae2887a953ebc65432e6162cba34b24e4abe
tomitobio <tomasetobio@gmail.com>
2023-10-27 00:22:51 -0300

    Update Main3.java

--- Java/Ejercicios Simples/Pingui/Main3.java
+++ Java/Ejercicios Simples/Pingui/Main3.java
@@ -1,30 +1,59 @@
-// Definir la interfaz Ave
+// // Definir la interfaz Ave
+// interface Ave {
+//     void volar();
+// }
+
+// // Definir la interfaz Pez
+// interface Pez {
+//     void nadar();
+// }
+
+// // Clase Pingüino que implementa ambas interfaces
+// class Pingüino implements Ave, Pez {
+//     @Override
+//     public void volar() {
+//         System.out.println("El pingüino no puede volar, ¡pero puede nadar!");
+//     }
+
+//     @Override
+//     public void nadar() {
+//         System.out.println("El pingüino está nadando en el agua.");
+//     }
+// }
+
+// public class Main3 {
+//     public static void main(String[] args) {
+//         Pingüino miPingüino = new Pingüino();
+//         miPingüino.volar();
+//         miPingüino.nadar();
+//     }
+// }
+
 interface Ave {
     void volar();
 }
 
-// Definir la interfaz Pez
 interface Pez {
     void nadar();
 }
 
-// Clase Pingüino que implementa ambas interfaces
-class Pingüino implements Ave, Pez {
+class Pinguino implements Ave, Pez {
     @Override
     public void volar() {
-        System.out.println("El pingüino no puede volar, ¡pero puede nadar!");
+        System.out.println("Esta ave esta volando!!");
     }
 
     @Override
     public void nadar() {
-        System.out.println("El pingüino está nadando en el agua.");
+        System.out.println("Esta pez esta nadando!!");
     }
 }
 
 public class Main3 {
     public static void main(String[] args) {
-        Pingüino miPingüino = new Pingüino();
-        miPingüino.volar();
-        miPingüino.nadar();
+        Pinguino pingui = new Pinguino();
+
+        pingui.volar();
+        pingui.nadar();
     }
 }
